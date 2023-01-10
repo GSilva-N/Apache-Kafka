cf769e3fbc70569a5dbccb3309c54857d58f22dc
GSilva-N <guilherme.paganini10@gmail.com>
2023-01-09 22:43:33 -0300

    push inicial

---
+++ Kafka.Course/Kafka.Course.Producer/Program - Cópia.cs
@@ -0,0 +1,28 @@
+﻿//using Confluent.Kafka;
+
+
+//var config = new ProducerConfig { BootstrapServers = "localhost:9092", Acks=Acks.Leader };
+
+//using var producer = new ProducerBuilder<Null, string>(config).Build();
+//try
+//{
+//    //for (int i = 0; i < 10; i++)
+//    {
+//        var response = producer.ProduceAsync("first_topic",
+//            new Message<Null, string> { Value = "Producer .NET " }).GetAwaiter().GetResult();
+
+//        Console.WriteLine(
+//            $"Topico: {response.Topic},\n" +
+//            $"Partição: {response.Partition.Value},\n" +
+//            $"Offset: {response.Offset},\n" +
+//            $"Timestamp: {response.Timestamp.UtcDateTime}, \n"
+//            );
+//    }
+//    producer.Flush();
+
+//}
+//catch (Exception)
+//{
+
+//    throw;
+//}
