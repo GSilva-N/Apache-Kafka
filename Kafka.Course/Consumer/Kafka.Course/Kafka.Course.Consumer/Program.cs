3eccd0170986afe49bed01550b8d5cc669d52d87
GSilva-N <guilherme.paganini10@gmail.com>
2023-01-10 23:21:57 -0300

    Criando Consumer

---
+++ Kafka.Course/Consumer/Kafka.Course/Kafka.Course.Consumer/Program.cs
@@ -0,0 +1,42 @@
+﻿using Confluent.Kafka;
+
+var config = new ConsumerConfig
+{
+    GroupId = "dotenet-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+    //PartitionAssignmentStrategy = PartitionAssignmentStrategy.CooperativeSticky
+};
+
+
+using var consumer = new ConsumerBuilder<string, string>(config).Build();
+
+consumer.Subscribe("first_topic");
+
+CancellationTokenSource token = new();
+
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+
+        if (response.Message != null)
+        {
+            Console.WriteLine(
+            $"Topico: {response.Topic},\n" +
+            $"Mensagem: {response.Message.Value},\n" +
+            $"Key: {response.Message.Key},\n" +
+            $"Partição: {response.Partition.Value},\n" +
+            $"Offset: {response.Offset},\n" +
+            $"Timestamp: {response.Timestamp.UtcDateTime} \n");
+        }
+
+        Console.WriteLine("...");
+    }
+}
+catch (ConsumeException)
+{
+    throw;
+}
+
---
+++ Kafka.Course/Producer/Kafka.Course.Producer/Program.cs
@@ -0,0 +1,37 @@
+﻿using Confluent.Kafka;
+
+
+var config = new ProducerConfig { BootstrapServers = "localhost:9092", Acks=Acks.Leader };
+
+using var producer = new ProducerBuilder<string, string>(config).Build();
+try
+{   
+
+    for (int i = 0; i < 10; i++)
+    {
+        var key = $"id_{i}";
+
+
+        var response = producer.ProduceAsync("first_topic",
+            new Message<string, string> { 
+                Value = "Kafka Producer "+i, 
+                Key = key })
+            .GetAwaiter()
+            .GetResult();
+
+        Console.WriteLine(
+            $"Topico: {response.Topic},\n" +
+            $"Key: {response.Key},\n" +
+            $"Partição: {response.Partition.Value},\n" +
+            $"Offset: {response.Offset},\n" +
+            $"Timestamp: {response.Timestamp.UtcDateTime} \n"
+            );
+    }
+    producer.Flush();
+
+}
+catch (Exception)
+{
+
+    throw;
+}
